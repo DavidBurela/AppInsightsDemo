68952bb21c666011c8775c3607d26eb8e11e85eb
David Burela <David.Burela@gmail.com>
2015-08-18 14:28:21 +1000

    Created a new project

---
+++ AppInsightsDemo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppInsightsDemo2.Startup))]
+namespace AppInsightsDemo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
